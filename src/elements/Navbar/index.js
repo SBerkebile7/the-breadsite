3d280d5ae0ec1dba6734fbd223f7eccc44e823a1
Spenny Berks <sberkebile@live.com>
2021-12-10 14:58:27 -0500

    more nav work

--- src/elements/Navbar/index.js
+++ src/elements/Navbar/index.js
@@ -6,10 +6,17 @@ function Navbar() {
         <div class="wrap">
             <header id="header">
                 <div class="container-fluid">
-                    <nav class="navbar navbar-expand-sm navbar-light bg-light" id="navbar" >
+                    <nav class="navbar navbar-expand-sm navbar-light bg-light" id="navbar">
                         <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
-                            <div class="collapse navbar-collapse text-center" >
-                                <ul class="navbar-nav" >
+                            <div class="text-left">
+                                <ul class="navbar-nav">
+                                    <li class="mx-5 px-5">
+                                        The Breadsite
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="collapse navbar-collapse text-center">
+                                <ul class="navbar-nav">
                                     <li class="mx-3">
                                         <Link to="/" class="text-decoration-none">
                                             Home                
